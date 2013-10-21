52054ab5a36de49bd6af516d372375a68783aa67
user <user@debian.compute.dtu.dk>
2013-10-21 09:07:21 +0200

    memory exerciser unit test is added

--- check_mm.c
+++ check_mm.c
@@ -25,9 +25,18 @@
  * http://check.sourceforge.net/doc/check_html/check_4.html#Convenience-Test-Functions
  *
  */
-
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <check.h>
 #include "mm.h"
+
+/* Print debug messages for each alloc/free attempt */
+#define VERBOSE_OUTPUT 0
+
+/* Verify 32 byte alignment */
+#define CHECK_ALIGNMENT 1
+
 
 /** 
  * @name   Example simple allocation unit test 
@@ -184,6 +193,176 @@ START_TEST (test_6) {
 }
 END_TEST
 
+/* XOR a block of memory */
+uint32_t sum_block(uint32_t *data, uint32_t size) {
+  uint32_t sum = 0;
+  int n;
+  for (n=0; n < (size-31) >> 2; n++) {
+    sum ^= data[n];
+  }
+  return sum;
+}
+
+/**
+ * @name   Memory exerciser
+ * @brief  Allocate and use memory blocks of varying sizes.
+ * 
+ *  When adding the test case to a test suite, remember to add:
+ *
+ *  tcase_set_timeout(tc_core, 0);
+ *  tcase_add_test (tc_core, test_memory_exerciser);
+ *
+ *
+ */
+START_TEST (test_memory_exerciser) {
+  uint32_t iterations = 1000; /* Alter as required */
+
+  /* Struct to keep track of allocations */
+  struct {
+    void *addr;     /* Pointer returned by alloc */ 
+    uint32_t *data; /* Pointer used for accessing region (will differ
+		       from "addr" if alignment is handled by test) */
+    uint32_t size;  /* Size of requested block */
+    uint32_t crc;   /* Checksum of contents of block */
+  } blocks[16];
+  
+  uint32_t  clock;
+  uint32_t  total_memory_size=0;
+  uint32_t  n;
+
+  for(clock=0; clock<16; clock++) {
+    blocks[clock].addr=0;
+  }
+  
+  clock=0;
+  
+  while(iterations--) {
+    char *addr;
+    
+    /* randomize the size of a block. */
+    blocks[clock].size=(24*1024*1024-total_memory_size)*(rand()&(1024*1024-1))/
+      (1024*8);
+    
+    /* Sanity check the block size. */
+    if ((blocks[clock].size>0) && 
+	(blocks[clock].size<(24*1024*1024))) {
+
+      /* Try to allocate memory. */
+      addr = embedded_malloc(blocks[clock].size);
+
+      /* Check if it was successful. */
+      ck_assert_msg(addr != NULL, "Memory allocation failed!");
+
+
+#if CHECK_ALIGNMENT
+      /* This address should already be 32 byte aligned */
+      blocks[clock].data = (uint32_t *) addr;
+#else
+      /* When not implemented, we align it ourselves. */
+      blocks[clock].data = (uint32_t *) ((uint32_t) (addr + 31) & (uint32_t) ~0x1f);
+      ck_assert ((char *) blocks[clock].data >= addr);
+#endif
+
+      if ((uint32_t) blocks[clock].data & 0x1f) {
+	printf("Unaligned address %p (of %p) returned!\n", blocks[clock].data, addr);
+	ck_assert(0);
+      }
+
+#if VERBOSE_OUTPUT
+	printf("alloc[%02d] %d bytes, total=%d\n", clock, blocks[clock].size, total_memory_size);
+#endif
+
+      /* Fill memory with data for verification */
+      {
+	uint32_t sum = 0;
+	uint32_t x;
+	for (n=0; n < (blocks[clock].size-31) >> 2; n++) {
+	  x = rand();
+	  blocks[clock].data[n] = x;
+	  sum ^= x;
+	}
+	blocks[clock].crc = sum;
+      }
+
+      /* Keep track of how much memory we have allocated... */
+      total_memory_size+=blocks[clock].size;
+
+      /* and the address. */
+      blocks[clock].addr=addr;
+    }
+    else {
+      blocks[clock].addr=0;
+    }
+
+    /* Move on to next block */
+    clock=(clock+1)&15;
+
+    /* Verify all existing blocks before free */
+    {
+      uint32_t all_ok = 1;
+      for (n=0; n < 16; n++) {
+	if (blocks[n].addr != NULL) {
+	  uint32_t sum = sum_block(blocks[n].data, blocks[n].size);
+
+	  if (blocks[n].crc != sum) {
+	    printf("Checksum failed for block %d at addr=%p: %08x != %08x\n", 
+		   n, blocks[n].addr, blocks[n].crc, sum);
+	    all_ok = 0;
+	  }
+	}
+      }
+      ck_assert_msg(all_ok, "Pre-free memory block corruption found\n");
+    }
+
+	 
+    /* Try to free one block. */
+    if (0 != blocks[clock].addr) {
+#if VERBOSE_OUTPUT
+      printf("free [%02d] %d bytes\n", clock, blocks[clock].size);
+#endif
+
+      embedded_free(blocks[clock].addr);
+      total_memory_size-=blocks[clock].size;
+
+      /* Mark block as free */
+      blocks[clock].addr = NULL;
+
+      /* Verify all existing blocks after free */
+      {
+	uint32_t all_ok = 1;
+	for (n=0; n < 16; n++) {
+	  if (blocks[n].addr != NULL) {
+	    uint32_t sum = sum_block(blocks[n].data, blocks[n].size);
+
+	    if (blocks[n].crc != sum) {
+	      printf("Checksum failed for block %d at addr=%p: %08x != %08x\n", 
+		     n, blocks[n].addr, blocks[n].crc, sum);
+	      all_ok = 0;
+	    }
+	  }
+	}
+	ck_assert_msg(all_ok, "Post-free memory block corruption found\n");
+      }
+    }
+  }
+
+  /* Free final blocks */
+  for (clock=0; clock < 16; clock++) {
+    if (blocks[clock].addr != NULL) {
+#if VERBOSE_OUTPUT
+      printf("free [%02d] %d bytes\n", clock, blocks[clock].size);
+#endif
+      uint32_t sum = sum_block(blocks[clock].data, blocks[clock].size);
+
+      if (blocks[clock].crc != sum) {
+	printf("Checksum failed for block %d: %08x != %08x\n", clock, blocks[clock].crc, sum);
+	ck_assert(0);
+      }
+      embedded_free(blocks[clock].addr);
+    }
+  }
+}
+END_TEST
 
 /**
  * @name   Example unit test suite.
@@ -202,7 +381,7 @@ Suite* embedded_malloc_suite() {
    tcase_add_test (tc_core, test_4);
    tcase_add_test (tc_core, test_5);
    tcase_add_test (tc_core, test_6);
- 
+   tcase_add_test (tc_core, test_memory_exerciser);
 
 
   suite_add_tcase(s, tc_core);
